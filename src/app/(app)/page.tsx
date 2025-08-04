d5bb02bad0866d3cf707aa501272bccefbeea84f
Jobie <engrjvramos@gmail.com>
2025-08-04 14:25:56 +0800

    chore: add under construction page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,12 +1,10 @@
-import ThemeToggler from '@/components/theme-toggler';
-
 export default function Home() {
   return (
-    <div className="">
-      <ThemeToggler />
-
-      <h1 className="text-4xl">Jose Roberto Vasquez Ramos</h1>
-      <h2>Frontend Web Developer</h2>
+    <div className="flex h-screen items-center justify-center bg-black px-4 text-center text-white">
+      <div>
+        <h1 className="text-3xl font-bold">🚧 jobie.dev is under construction</h1>
+        <p className="text-muted-foreground mt-2">I&apos;m building something awesome. Check back soon!</p>
+      </div>
     </div>
   );
 }
