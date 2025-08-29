6f58694032af3d9233390b4936f44c26d9786cbe
Jobie <engrjvramos@gmail.com>
2025-08-29 10:30:03 +0800

    chore: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from 'next';
 import { Host_Grotesk, Onest } from 'next/font/google';
 import './globals.css';
@@ -65,6 +66,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
